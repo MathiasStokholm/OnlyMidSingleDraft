1d1a64559c34d6b0ee47ddb1951be584d1353fe5
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2019-10-13 23:12:14 +0200

    Updates

---
+++ precache-manifest.15aa5d5b77e11cf2f8cd1f50e925ce1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a643c3b3409a4401883fa7669fa29dc",
+    "url": "/OnlyMidSingleDraft/index.html"
+  },
+  {
+    "revision": "b2f89c847d0816f285bc",
+    "url": "/OnlyMidSingleDraft/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "43d6e85f1bc4ccc17ddc",
+    "url": "/OnlyMidSingleDraft/static/css/main.e1a01371.chunk.css"
+  },
+  {
+    "revision": "b2f89c847d0816f285bc",
+    "url": "/OnlyMidSingleDraft/static/js/2.10c1e665.chunk.js"
+  },
+  {
+    "revision": "43d6e85f1bc4ccc17ddc",
+    "url": "/OnlyMidSingleDraft/static/js/main.dd9e3211.chunk.js"
+  },
+  {
+    "revision": "b5b630939ac086623748",
+    "url": "/OnlyMidSingleDraft/static/js/runtime-main.27d84cfd.js"
+  }
+]);
