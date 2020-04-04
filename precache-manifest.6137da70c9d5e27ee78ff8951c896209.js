3d22c18d5b9d497c037ac43143b4f4b7930abceb
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2020-04-05 00:49:08 +0200

    Updates

---
+++ precache-manifest.6137da70c9d5e27ee78ff8951c896209.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814ca6cfbbcaa1060855da0cd4d1419d",
+    "url": "/OnlyMidSingleDraft/index.html"
+  },
+  {
+    "revision": "8185726520d0f2bcd09f",
+    "url": "/OnlyMidSingleDraft/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "db2af271dbf9804136ea",
+    "url": "/OnlyMidSingleDraft/static/css/main.36792666.chunk.css"
+  },
+  {
+    "revision": "8185726520d0f2bcd09f",
+    "url": "/OnlyMidSingleDraft/static/js/2.8575f05d.chunk.js"
+  },
+  {
+    "revision": "862679d2d2c47ad68b55484ef8c78518",
+    "url": "/OnlyMidSingleDraft/static/js/2.8575f05d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2af271dbf9804136ea",
+    "url": "/OnlyMidSingleDraft/static/js/main.5acc5668.chunk.js"
+  },
+  {
+    "revision": "9fc15e0d5329272bde60",
+    "url": "/OnlyMidSingleDraft/static/js/runtime-main.613f2ab5.js"
+  }
+]);
