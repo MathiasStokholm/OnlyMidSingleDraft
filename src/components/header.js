8130212e5d5ccf7a02d6cda17ff1909ffe7f1558
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2020-04-05 00:09:04 +0200

    Added new game modal

--- src/components/header.js
+++ src/components/header.js
@@ -1,41 +1,68 @@
 import React from 'react'
-import {Link, withRouter} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {
     Navbar,
     NavbarToggler,
     NavbarBrand,
     Nav,
     NavItem,
-    NavLink
+    NavLink, Modal, ModalHeader, ModalFooter, Button
 } from 'reactstrap';
 
 class Header extends React.Component {
-    startNewGame() {
-        // TODO: Add dialog here to avoid mistakes when clicking new game
-        this.props.backend.startNewGame();
+    constructor(props) {
+        super(props);
+        this.state = {
+            newGameDialogShowing: false
+        };
+    }
+
+    toggleNewGameDialog() {
+        this.setState({
+            newGameDialogShowing: !this.state.newGameDialogShowing
+        })
+    }
+
+    startNewGameClicked() {
+        this.setState({
+            newGameDialogShowing: true
+        })
     }
 
     render() {
         const heroStatsReady = this.props.heroStats != null;
 
         return (
-            <Navbar color="dark" className="navbar-dark" expand="md" style={{marginBottom: '5px'}}>
-                <NavbarBrand tag={Link} to="/">Only Mid Single Draft</NavbarBrand>
-                <NavbarToggler onClick={this.toggle}/>
-                <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <NavLink tag={Link} to="/old">Old App</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink tag={Link} to="/" disabled={!heroStatsReady} onClick={(event) => {
-                            event.preventDefault();
-                            this.startNewGame();
-                        }}>Start New Game</NavLink>
-                    </NavItem>
-                </Nav>
-            </Navbar>
+            <div>
+                <Modal isOpen={this.state.newGameDialogShowing} className={this.props.className}
+                       toggle={() => this.toggleNewGameDialog()}>
+                    <ModalHeader>Really start new game?</ModalHeader>
+                    <ModalFooter>
+                        <Button color="primary" onClick={() => {
+                            this.props.backend.startNewGame();
+                            this.toggleNewGameDialog();
+                        }}>Yes!</Button>
+                        <Button color="muted" onClick={() => this.toggleNewGameDialog()}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+                <Navbar color="dark" className="navbar-dark" expand="md" style={{marginBottom: '5px'}}>
+                    <NavbarBrand tag={Link} to="/">Only Mid Single Draft</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/old">Old App</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/" disabled={!heroStatsReady} onClick={(event) => {
+                                event.preventDefault();
+                                this.startNewGameClicked();
+                            }}>Start New Game</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            </div>
         );
     }
 }
 
-export default withRouter(Header)
+export default Header
